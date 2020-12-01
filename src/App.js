ac0b8b5794ab05104994cd597185b59736891328
Emily <linemily97@gmail.com>
2020-11-30 19:21:44 -0800

    testing edited, added to App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Learn React</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-test='component-app'>
+      App
+      <div data-test='counter-display'>
+        the counter is currently &nbsp;
+        <span data-test='count'>{count}</span>
+      </div>
+      <button data-test='increment-button' onClick={() => setCount(count + 1)}>
+        Increment Counter
+      </button>
     </div>
+    // <div className='App'>
+    //   <header className='App-header'>
+    //     <img src={logo} className='App-logo' alt='logo' />
+    //     <p>Learn React</p>
+    //     <a
+    //       className='App-link'
+    //       href='https://reactjs.org'
+    //       target='_blank'
+    //       rel='noopener noreferrer'
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,60 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+//we're using enzyme not testing library ehre
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//set up enzyme's adaptor
+Enzyme.configure({ adapter: new Adapter() });
+
+//for TDD we want red tests before green tests
+
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
 });
+test('renders button', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+});
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
+});
+test('counter starts at 0', () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, 'count').text();
+  expect(count).toBe('0');
+});
+test('clicking on button increments counter', () => {
+  const wrapper = setup();
+
+  //find the button
+  const button = findByTestAttr(wrapper, 'increment-button');
+  //click the button
+  button.simulate('click');
+  //find the display, and test that the number has been incremented
+  const count = findByTestAttr(wrapper, 'count').text();
+  expect(count).toBe('1');
+});
+// test('renders without crashing', () => {
+//   const wrapper = shallow(<App />);
+//   // expect(wrapper).toBeFalsy();
+//   // expect(wrapper).toBeTruthy();
+
+//   console.log(wrapper.debug());
+//   // throw new Error;
+// });
+// // test('renders learn react link', () => {
+// //   render(<App />);
+// //   const linkElement = screen.getByText(/learn react/i);
+// //   expect(linkElement).toBeInTheDocument();
+// //   // throw new Error();
+// // });
